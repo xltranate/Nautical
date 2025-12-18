ad7e6c821f43bd17d098e6922a55a791e1984b17
xltranate <109490633+xltranate@users.noreply.github.com>
2025-12-17 19:56:36 -0500

    Create favicon.js

---
+++ favicon.js
@@ -0,0 +1,6 @@
+// favicon.js
+const link = document.createElement('link');
+link.rel = 'icon';
+link.href = '/favicon.ico'; // path to your favicon in root
+link.type = 'image/x-icon';
+document.head.appendChild(link);
