2009eacbe500f182fb8d170a09231abf20692afb
xltranate <109490633+xltranate@users.noreply.github.com>
2025-12-17 20:06:02 -0500

    Delete favicon.js

--- favicon.js
+++
@@ -1,6 +0,0 @@
-// favicon.js
-const link = document.createElement('link');
-link.rel = 'icon';
-link.href = '/favicon.ico'; // path to your favicon in root
-link.type = 'image/x-icon';
-document.head.appendChild(link);
